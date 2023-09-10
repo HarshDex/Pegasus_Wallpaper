a98ecd89a468c79dcb82b4a8b976b0500c218e1c
Jatin Singh Bhandari <102027407+dreamcatstudios@users.noreply.github.com>
2023-09-10 14:15:12 +0530

    added authetication

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,37 @@
 import React from "react";
-
 import "./Home.css";
 import Navigation from "../components/Navigation";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Home = () => {
+  const { isAuthenticated, user } = useAuth0();
+
   return (
     <div className="hero--section">
-      <Navigation/>
+      <Navigation />
       <div className="center--container">
         <div className="center--wrap">
-          <h1>Pegasus Wallpapers</h1>
-          <div className="search--container">
-            <input
-              className="search--box"
-              type="text"
-              placeholder="Search for Photos"
-            />
-            <button>
-              <p>Search</p>
-            </button>
-          </div>
-          <div id="suggestion">
-            
-          </div>
+          {isAuthenticated ? (
+            <>
+              <h1>Pegasus Wallpapers</h1>
+              <div className="search--container">
+                <input
+                  className="search--box"
+                  type="text"
+                  placeholder="Search for Photos"
+                />
+                <button>
+                  <p>Search</p>
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+              <h1>Login to get access to our highest quality wallpapers.</h1>
+              <div className="search--container">    
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
